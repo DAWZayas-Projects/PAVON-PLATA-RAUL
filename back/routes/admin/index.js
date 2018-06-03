e3a000b632f99e00fe94dfadae07a894852929da
Raúl Pavón <raulpavonfp@gmail.com>
2018-06-03 13:28:55 +0200

    Added functionality to generate random posts

--- back/routes/admin/index.js
+++ back/routes/admin/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const faker = require('faker');
 const router = express.Router();
+const Post = require('../../models/Post');
 
 router.all('/*', (req, res, next) => {
     req.app.locals.layout = 'admin';
@@ -10,7 +12,25 @@ router.get('/', (req,res) => {
     return res.render('admin/index');
 });
 
+router.post('/generate-fake-posts', (req, res) => {
 
+    for (let i = 0; i < req.body.amount; i++) {
+        let post = new Post({
+            title: faker.name.title(),
+            status: faker.random.arrayElement(['publico', 'draft', 'privado']),
+            allowComments: Math.round(Math.random() + 1) % 2 === 0,
+            body: faker.lorem.paragraph(),
+        });
+
+        post.save().then(createdPost => {
+            console.log('Post created: ' + createdPost)
+        }).catch(error => console.error(error));
+
+    }
+
+    return res.redirect('/admin/posts');
+
+});
 
 
 module.exports = router;
