23e5b72108dc07cd84beabbdd6a13e0a95ce061c
Raúl Pavón <raulpavonfp@gmail.com>
2018-06-16 15:22:19 +0200

    changed database config

--- back/config/database.js
+++ back/config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoDbUrl: 'mongodb://localhost:27017/cms'
+    //mongoDbUrl: 'mongodb://localhost:27017/cms'
+	mongoDbUrl: 'mongodb://cms-user:cmsuser95@ds261660.mlab.com:61660/greywardencms'
 };
